3298202d3d4387e2f0835e1480e4e78bbdd224fe
HaesooJung <164863432+HaesooJung@users.noreply.github.com>
2024-05-10 02:30:59 +0900

    feat. 회원가입 쿠키 문제 해결

--- server.js
+++ server.js
@@ -2,28 +2,38 @@ const express = require('express')  //express라이브러리 사용하겠다는 
 const app = express()    
 const {MongoClient, ObjectId} = require('mongodb')  // mongoDB 연결, objectID사용
 const cors = require('cors')
+const MongoStore = require('connect-mongo')
+const nodemailer = require('nodemailer');
+
 
 
 app.use(express.static(__dirname + '/public'))  // 퍼블릭 폴더 내부의 파일을 사용할 수 있음!
 //app.set('view engine', 'ejs')  // 우린 ejs를 view engine으로 쓴다
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) // 이게 있어야 요청.body 사용 편해짐!
-app.use('/', require('./routes/sign.js') )
 app.use(cors());
 
 
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-
 app.use(passport.initialize())
+
+
+
 app.use(session({
   secret: '0920',
   resave : false,
-  saveUninitialized : false
+  saveUninitialized : false,
+  cookie : {maxAge : 1000 * 60 * 3000},
+  store: MongoStore.create({
+  mongoUrl : 'mongodb+srv://codra:qwer1234@codra.j81aade.mongodb.net/?retryWrites=true&w=majority&appName=Codra',
+  dbName: 'Darak',
+  })
 }))
-
 app.use(passport.session()) 
+
+
 
 
 passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
@@ -77,21 +87,161 @@ app.listen(5173, () => {    //포트번호 5173
 
 
 
-app.get('/', (req, res) => {res.status(200)
+
+app.post('/signin', (req, res, next) => {
+  
+
+  passport.authenticate('local', (error, user, info) => {
+      if(error) return res.status(500).json(error)
+      if(!user) return res.status(401).json(info.message)
+      
+      // 요청 객체에 login 함수가 없는지 확인하여, next 함수 대신 에러를 처리합니다.
+      if (!req.logIn) {
+          return res.status(500).json({ message: '로그인 기능을 찾을 수 없습니다' });
+      }
+
+      req.logIn(user, (err) => {
+          if (err) return next(err);
+          res.status(200) // next 함수를 여기서 사용
+          res.end()
+
+      });
+  })(req, res, next);
+});
+
+
+app.get('/signin', async(req, res) =>{
+  res.status(200).end()
+}
+
+
+)
+
+app.post('/signup', async (req, res, next) => {
+      if(await db.collection('user').findOne({username : req.body.username}))
+        {
+          res.status(500)
+          console.error('Invalid user name!')
+          res.json({ message : "이미 사용 중인 이메일이 있어요."})
+          res.end()
+          return;
+        }
+  
+        else if(await db.collection('user').findOne({nickname : req.body.nickname}))
+          {
+            res.status(500)
+            console.error('Invalid nickname!')
+            res.json({ message : "이미 사용 중인 닉네임이 있어요."})
+            res.end()
+            return;
+          }
+    
+        
+        const result = { username : req.body.username,
+          name : req.body.name,
+          password : req.body.password,
+          nickname : req.body.nickname
+        }
+        await db.collection('user').insertOne(result)
+    
+        const createdOne = await db.collection('user').findOne({username : req.body.username})
+        res.status(200)
+        console.log(req.body.username)
+        console.log(req.body.password)
+        res.end()
+  
+  
+  
+    
+
+    })
+
+
+
+
+    function loggedin(req, res, next) {
+      if (req.user) {
+        console.log('not expired!')
+          next();
+      }
+      else{
+          res.status(500)
+          throw new Error('Session expired!')
+  
+      }
+    } 
+
+
+app.get('/main', (req, res) => {
+  console.log(req.user._id)
+
+  res.json({
+
+
+    UID : user._id,
+    username : user.username,
+    nickname : user.nickname
+
+
+
+
+  })
+  res.status(200)
   res.end();
   ;})
 
 
 
+///////////////////////////////비밀번호 난수 생성 ///////////////////
+var variable = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
+
+      //비밀번호 랜덤 함수
+function createRandomPassword(variable, passwordLength) {
+var randomString = "";
+for (var j=0; j<passwordLength; j++) 
+randomString += variable[Math.floor(Math.random()*variable.length)];
+return randomString}
 
 
 
-function loggedin(req, res, next) {
-  if (req.user) {
-      next();
+const sendMail = async() =>{
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: { // 이메일을 보낼 계정 데이터 입력
+    user: 'wjdgotn0920@gmail.com',
+    pass: 'kqxk toes ulov kwsj',
+  },
+});
+
+
+
+const emailOptions = { // 옵션값 설정
+    from: 'wjdgotn0920@gmail.com',
+    to: k/**/,
+    subject: '다락에서 임시비밀번호를 알려드립니다.',
+    html: 
+    "<h1 >다락에서 새로운 비밀번호를 알려드립니다.</h1> <h2> 비밀번호 : " + targetPassword + "</h2>"
+    +'<h3 style="color: crimson;">임시 비밀번호로 로그인 하신 후, 반드시 비밀번호를 수정해 주세요.</h3>'}
+  
+  
+  await transporter.sendMail(emailOptions)
+  
   }
-  else{
-      res.status(500)
-      throw new Error('Session expired!')
-  }
-} 
+
+
+
+
+app.post('/new_password', async (req, res) =>{
+
+  targetAccount = req.body.username
+  targetPassword = createRandomPassword(variable, 8)
+
+  realInfo = await db.collection('user').findOne({username : req.body.username})
+  if(realInfo){
+    await db.collection('post').updateOne({username : req.body.username}), {$set : {password : targetPassword}}
+    sendMail()
+    res.status(200).end()
+  
+  }})
